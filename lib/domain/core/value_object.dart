6f882e44a83355fc3c85812c4b12dbcac47b9b47
Jerry Tabernero <tabernerojerry@gmail.com>
2020-12-21 19:59:03 +0800

    auth value objects

---
+++ lib/domain/core/value_object.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_firebase_ddd/domain/core/failures.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
