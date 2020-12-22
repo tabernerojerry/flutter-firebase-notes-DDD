49f4f632c25df9e23cd229bdb074f3461823cdf8
Jerry Tabernero <tabernerojerry@gmail.com>
2020-12-22 14:23:33 +0800

    auth facade interface

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:notes_firebase_ddd/domain/auth/auth_failure.dart';
+import 'package:notes_firebase_ddd/domain/auth/value_objects.dart';
+
+abstract class IAuthFacade {
+  // Unit is a tuple which just a regular class (void)
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
