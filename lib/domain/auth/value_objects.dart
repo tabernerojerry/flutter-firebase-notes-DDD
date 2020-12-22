f2ed455174579b8c3ce644cc7af240af36fd81b0
Jerry Tabernero <tabernerojerry@gmail.com>
2020-12-22 13:46:23 +0800

    renamed value objects in core

--- lib/domain/auth/value_objects.dart
+++ lib/domain/auth/value_objects.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:notes_firebase_ddd/domain/core/failures.dart';
 
-import 'package:notes_firebase_ddd/domain/core/value_object.dart';
+import 'package:notes_firebase_ddd/domain/core/value_objects.dart';
 import 'package:notes_firebase_ddd/domain/core/value_validators.dart';
 
 class EmailAddress extends ValueObject<String> {
