7bd744484904ef17d9e02582970ba88ebeb957d7
Jerry Tabernero <tabernerojerry@gmail.com>
2020-12-25 15:40:33 +0800

    note value failures

--- lib/presentation/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -1,4 +1,3 @@
-import 'package:flushbar/flushbar.dart';
 import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -131,6 +130,10 @@ class SignInForm extends StatelessWidget {
                   ),
                 ),
               ),
+              if (state.isSubmitting) ...[
+                const SizedBox(height: 8.0),
+                const LinearProgressIndicator(value: null),
+              ]
             ],
           ),
         );
