7f368fa21e9e5e044f3f41b963832bfb97a412ec
Jerry Tabernero <tabernerojerry@gmail.com>
2020-12-26 15:33:56 +0800

    note watcher and actor blocs

---
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes_firebase_ddd/domain/notes/i_note_repository.dart';
+import 'package:notes_firebase_ddd/domain/notes/note.dart';
+import 'package:notes_firebase_ddd/domain/notes/note_failure.dart';
+
+part 'note_actor_event.dart';
+part 'note_actor_state.dart';
+part 'note_actor_bloc.freezed.dart';
+
+class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
+  final INoteRepository _noteRepository;
+
+  NoteActorBloc(this._noteRepository) : super(const _Initial());
+
+  @override
+  Stream<NoteActorState> mapEventToState(
+    NoteActorEvent event,
+  ) async* {
+    yield const NoteActorState.actionInProgress();
+    final possibleFailure = await _noteRepository.delete(event.note);
+    yield possibleFailure.fold(
+      (failure) => NoteActorState.deleteFailure(failure),
+      (_) => const NoteActorState.deleteSuccess(),
+    );
+  }
+}
